cf761fb3d4364ac0b72114e2f512c749c4dd9a38
Nkosi Lawrence <trinilawrence7@hotmail.com>
2020-05-09 00:17:29 -0400

    Update sample.py
    sample.py now prints to file and stops after it crosses a specified
    file size. Updated README.md to reflecting sampling capabilities.

--- sample.py
+++ sample.py
@@ -7,6 +7,7 @@ import os
 import sys
 import glob
 from pathlib import Path
+from subprocess import Popen
 
 print('sample.py takes a model and generates sample output.')
 print('If you do not have a model file, close and run training.py')
@@ -42,7 +43,7 @@ if not option == 'q':
     # code for sampling here
     print(model_name + ' found.')
     first_model = '0'
-    model_count = len(glob.glob1(model_path, '*.0'))
+    model_count = len(glob.glob1(model_path, '*.0')) - 1
     print('Between ' + first_model + ' and ' + str(model_count) +
           ', choose which model you would to sample from.')
     print('If you would like to exit, type "q".')
@@ -50,8 +51,9 @@ if not option == 'q':
 
     if not model_choice == 'q':
         # currently limit length of output by lenght parameter
-        if not Path('samples/').exists():
-            os.mkdir('samples')
+        if not Path('samples/' + model_name + '/').exists():
+            if not Path ('samples/').exists():
+                os.mkdir('samples')
             os.mkdir('samples/' + model_name)
 
         sample_path = ('samples/' + model_name + '/')
@@ -64,23 +66,42 @@ if not option == 'q':
                 model_choice + '_' + str(sample_count)
             print()
 
+        print('How large do you want the ' + sample_name + '.txt to be?')
+        print('This script may take a while to run and uses all available CPU resoucres, '
+              'so choose a resonable amount.')
+        print('This value is in bytes. The default is 65536 B (64KB).')
+        print('Do you want to specify MB or KB? Press enter to accept default')
+        print('1. KB')
+        print('2. MB')
+        size_option = input(': ')
+        print()
+
+        if size_option == '1':
+            mb_size = input('Input size: ')
+            max_size = mb_size * 1024
+            print(max_size)
+            input()
+
+        if size_option == '2':
+            max_size = input('Input size:')
+            print()
+
+        if size_option == '':
+            max_size = '65536'
+
         print('Writing to ' + sample_name + '.txt')
-        # with open(sample_path + sample_name + '.txt', 'wb') as Output:
-        max_size = 1
-        stdout = os.system('python3 -B pytorch-rnn/sample.py --checkpoint '
-                           + model_path + model_name + '_' + model_choice +
-                           '.json > ' + sample_path + sample_name + '.txt')
-        if stdout.tell() > max_size:
-            sys.exit()
-        # for
-        #     if Output.tell() > max_size:
-        #         break
-        print('Finished')
-        # Output.write(sys.stdout)
-        # os.system('python3 -B pytorch-rnn/sample.py --checkpoint '
-        #           + model_path + model_name + model_choice +
-        #           '.json > ' + sample_path + sample_name + '.txt'
-        #           ' | ')
+        with open(sample_path + sample_name + '.txt', 'wb') as sample_file:
+            sample_write = Popen('python3 -B pytorch-rnn/sample.py --checkpoint '
+                                 + model_path + model_name + '_' + model_choice +
+                                 '.json', stdout=sample_file)
+            file_size = 0
+            while file_size < int(max_size):
+                file_size = sample_file.tell()
+            if sample_file.tell() > max_size:
+                Popen.terminate(sample_write)
+            sample_file.close()
+
+        print('Sample successful: ' + sample_name + '.txt')
 
 
 sys.exit()

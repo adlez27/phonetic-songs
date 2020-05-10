8e9866f6e87c2d411a943119056a3a3334df614e
Nkosi Lawrence <trinilawrence7@hotmail.com>
2020-05-09 20:41:36 -0400

    Update main.py
    As the version of torch and torchvision isn't readily available through
    pip, a dependency check was added to prompt the user whether or not
    they are going to be doing model training, and gives the option to
    install if in the affirmative.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-"""main.py ensures that the dependencies for converter.py and
-fetcher.py are installed, and allows you to start automatically
-if need be.
+"""main.py ensures that the dependencies for all scripts are
+installed, and allows you to start automatically if need be.
 
 This file should be run before the aforementioned, and updated
 whenever the dependencies change.
@@ -10,16 +9,102 @@ import sys
 
 print('Installing dependencies...')
 os.system('pip3 -q install -r requirements.txt')
-print('Dependencies installed...')
+
+try:
+    import torch
+    import torchvision
+except ImportError:
+    print('torch and torchvision are required for training '
+          'and sampling models.')
+    print('Are you going to be training and sampling models?')
+    torch_choice = input('y/n: ')
+    print()
+
+    while not torch_choice in ['y', 'n']:
+        print('Please specify.')
+        torch_choice = input('y/n: ')
+        if torch_choice == 'n':
+            print()
+            continue
+        print()
+
+    if torch_choice == 'y':
+        print('Please specify the operating system you\'re using')
+        print('1. Linux')
+        print('2. MacOS')
+        print('3. Windows')
+        os_choice = input(': ')
+        print()
+
+        # Linux
+        if os_choice == '1':
+            print('Please specify if you want the CPU PyTorch '
+                  'or GPU (NVIDIA - CUDA) PyTorch.')
+            print('The GPU version allows you to do CPU training as well.')
+            print('a. PyTorch CPU')
+            print('b. PyTorch GPU (CUDA)')
+            torch_ver = input(': ')
+            print()
+
+            if torch_ver == 'a':
+                print('This would download torch and torchvision wheels '
+                      'and install them automatically.')
+                print('This may take a while depending on your internet '
+                      'connection.')
+                os.system('pip3 -q install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp37-cp37m-linux_x86_64.whl')
+                os.system('pip3 -q install https://download.pytorch.org/whl/cpu/torchvision-0.3.0-cp37-cp37m-linux_x86_64.whl')
+            if torch_ver == 'b':
+                print('This would download torch and torchvision wheels '
+                      'and install them automatically.')
+                print('This may take a while depending on your internet '
+                      'connection.')
+                os.system('pip3 -q install https://download.pytorch.org/whl/cu90/torch-1.1.0-cp37-cp37m-linux_x86_64.whl')
+                os.system('pip3 -q install https://download.pytorch.org/whl/cu90/torchvision-0.3.0-cp37-cp37m-manylinux1_x86_64.whl')
+
+        # MacOS
+        if os_choice == '2':
+            print('MacOS binaries don\'t support CUDA, install from '
+                  'source if CUDA is needed.')
+            print('This would download the CPU binaries for torch and '
+                  'torchvision and install them automatically.')
+            print('This may take a while depending on your internet '
+                  'connection.')
+            os.system('pip3 -q install torch==1.1.0 torchvision==0.3.0')
+
+        # Windows
+        if os_choice == '3':
+            print('Please specify if you want the CPU PyTorch '
+                  'or GPU (NVIDIA - CUDA) PyTorch.')
+            print('The GPU version allows you to do CPU training as well.')
+            print('a. PyTorch CPU')
+            print('b. PyTorch GPU (CUDA)')
+            torch_ver = input(': ')
+            if torch_ver == 'a':
+                print('This would download torch and torchvision wheels '
+                      'and install them automatically.')
+                print('This may take a while depending on your internet '
+                      'connection.')
+                os.system('pip3 -q install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp37-cp37m-win_amd64.whl')
+                os.system('pip3 -q install https://download.pytorch.org/whl/cpu/torchvision-0.3.0-cp37-cp37m-win_amd64.whl')
+            if torch_ver == 'b':
+                print('This would download torch and torchvision wheels '
+                      'and install them automatically.')
+                print('This may take a while depending on your internet '
+                      'connection.')
+                os.system('pip3 -q install https://download.pytorch.org/whl/cu90/torch-1.1.0-cp37-cp37m-win_amd64.whl')
+                os.system('pip3 -q install https://download.pytorch.org/whl/cu90/torchvision-0.3.0-cp37-cp37m-win_amd64.whl')
+
+print('Dependencies installed.')
 print()
 print('Welcome to The Phonetic Song Project.')
 print('If you encounter any bugs, feel free to report them at '
       'https://github.com/adlez27/phonetic-songs')
+print()
 print('What would you like to do?')
 print('1. Fetch Lyrics')
 print('2. Convert Lyrics')
 print('3. Fetch Billboard Charts')
-print('4. Train Model (This may take a while)')
+print('4. Train Model')
 print('5. Sample Output from Model')
 print('Type "q" to exit.')
 forward = input(': ')

1064f7a14a59b037f75dc0c88f49577d3e57aac4
Nkosi Lawrence <trinilawrence7@hotmail.com>
2020-05-10 18:01:16 -0400

    Comment out training references to training
    main.py does not show training or sampling options, and does not check
    for torch and torchvision during the dependency check. trainer.py and
    sampler.py will be left in the repo but is not guaranteed to work. Will
    sort that out in a later commit.

--- main.py
+++ main.py
@@ -8,91 +8,91 @@ import os
 import sys
 
 print('Installing dependencies...')
-os.system('pip3 -q install -r requirements.txt')
+os.system('python3 -m pip install -r requirements.txt')
 
-try:
-    import torch
-    import torchvision
-except ImportError:
-    print('torch and torchvision are required for training '
-          'and sampling models.')
-    print('Are you going to be training and sampling models?')
-    torch_choice = input('y/n: ')
-    print()
+# try:
+#     import torch
+#     # import torchvision
+# except ImportError:
+#     print('torch and torchvision are required for training '
+#           'and sampling models.')
+#     print('Are you going to be training and sampling models?')
+#     torch_choice = input('y/n: ')
+#     print()
 
-    while not torch_choice in ['y', 'n']:
-        print('Please specify.')
-        torch_choice = input('y/n: ')
-        if torch_choice == 'n':
-            print()
-            continue
-        print()
+#     while not torch_choice in ['y', 'n']:
+#         print('Please specify.')
+#         torch_choice = input('y/n: ')
+#         if torch_choice == 'n':
+#             print()
+#             continue
+#         print()
 
-    if torch_choice == 'y':
-        print('Please specify the operating system you\'re using')
-        print('1. Linux')
-        print('2. MacOS')
-        print('3. Windows')
-        os_choice = input(': ')
-        print()
+#     if torch_choice == 'y':
+#         print('Please specify the operating system you\'re using')
+#         print('1. Linux')
+#         print('2. MacOS')
+#         print('3. Windows')
+#         os_choice = input(': ')
+#         print()
 
-        # Linux
-        if os_choice == '1':
-            print('Please specify if you want the CPU PyTorch '
-                  'or GPU (NVIDIA - CUDA) PyTorch.')
-            print('The GPU version allows you to do CPU training as well.')
-            print('a. PyTorch CPU')
-            print('b. PyTorch GPU (CUDA)')
-            torch_ver = input(': ')
-            print()
+#         # Linux
+#         if os_choice == '1':
+#             print('Please specify if you want the CPU PyTorch '
+#                   'or GPU (NVIDIA - CUDA) PyTorch.')
+#             print('The GPU version allows you to do CPU training as well.')
+#             print('a. PyTorch CPU')
+#             print('b. PyTorch GPU (CUDA)')
+#             torch_ver = input(': ')
+#             print()
 
-            if torch_ver == 'a':
-                print('This would download torch and torchvision wheels '
-                      'and install them automatically.')
-                print('This may take a while depending on your internet '
-                      'connection.')
-                os.system('pip3 -q install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp37-cp37m-linux_x86_64.whl')
-                os.system('pip3 -q install https://download.pytorch.org/whl/cpu/torchvision-0.3.0-cp37-cp37m-linux_x86_64.whl')
-            if torch_ver == 'b':
-                print('This would download torch and torchvision wheels '
-                      'and install them automatically.')
-                print('This may take a while depending on your internet '
-                      'connection.')
-                os.system('pip3 -q install https://download.pytorch.org/whl/cu90/torch-1.1.0-cp37-cp37m-linux_x86_64.whl')
-                os.system('pip3 -q install https://download.pytorch.org/whl/cu90/torchvision-0.3.0-cp37-cp37m-manylinux1_x86_64.whl')
+#             if torch_ver == 'a':
+#                 print('This would download torch and torchvision wheels '
+#                       'and install them automatically.')
+#                 print('This may take a while depending on your internet '
+#                       'connection.')
+#                 os.system('python3 -m pip install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp37-cp37m-linux_x86_64.whl')
+#                 os.system('python3 -m pip install https://download.pytorch.org/whl/cpu/torchvision-0.3.0-cp37-cp37m-linux_x86_64.whl')
+#             if torch_ver == 'b':
+#                 print('This would download torch and torchvision wheels '
+#                       'and install them automatically.')
+#                 print('This may take a while depending on your internet '
+#                       'connection.')
+#                 os.system('python3 -m pip install https://download.pytorch.org/whl/cu90/torch-1.1.0-cp37-cp37m-linux_x86_64.whl')
+#                 os.system('python3 -m pip install https://download.pytorch.org/whl/cu90/torchvision-0.3.0-cp37-cp37m-manylinux1_x86_64.whl')
 
-        # MacOS
-        if os_choice == '2':
-            print('MacOS binaries don\'t support CUDA, install from '
-                  'source if CUDA is needed.')
-            print('This would download the CPU binaries for torch and '
-                  'torchvision and install them automatically.')
-            print('This may take a while depending on your internet '
-                  'connection.')
-            os.system('pip3 -q install torch==1.1.0 torchvision==0.3.0')
+#         # MacOS
+#         if os_choice == '2':
+#             print('MacOS binaries don\'t support CUDA, install from '
+#                   'source if CUDA is needed.')
+#             print('This would download the CPU binaries for torch and '
+#                   'torchvision and install them automatically.')
+#             print('This may take a while depending on your internet '
+#                   'connection.')
+#             os.system('python3 -m pip install torch==1.1.0 torchvision==0.3.0')
 
-        # Windows
-        if os_choice == '3':
-            print('Please specify if you want the CPU PyTorch '
-                  'or GPU (NVIDIA - CUDA) PyTorch.')
-            print('The GPU version allows you to do CPU training as well.')
-            print('a. PyTorch CPU')
-            print('b. PyTorch GPU (CUDA)')
-            torch_ver = input(': ')
-            if torch_ver == 'a':
-                print('This would download torch and torchvision wheels '
-                      'and install them automatically.')
-                print('This may take a while depending on your internet '
-                      'connection.')
-                os.system('pip3 -q install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp37-cp37m-win_amd64.whl')
-                os.system('pip3 -q install https://download.pytorch.org/whl/cpu/torchvision-0.3.0-cp37-cp37m-win_amd64.whl')
-            if torch_ver == 'b':
-                print('This would download torch and torchvision wheels '
-                      'and install them automatically.')
-                print('This may take a while depending on your internet '
-                      'connection.')
-                os.system('pip3 -q install https://download.pytorch.org/whl/cu90/torch-1.1.0-cp37-cp37m-win_amd64.whl')
-                os.system('pip3 -q install https://download.pytorch.org/whl/cu90/torchvision-0.3.0-cp37-cp37m-win_amd64.whl')
+#         # Windows
+#         if os_choice == '3':
+#             print('Please specify if you want the CPU PyTorch '
+#                   'or GPU (NVIDIA - CUDA) PyTorch.')
+#             print('The GPU version allows you to do CPU training as well.')
+#             print('a. PyTorch CPU')
+#             print('b. PyTorch GPU (CUDA)')
+#             torch_ver = input(': ')
+#             if torch_ver == 'a':
+#                 print('This would download torch and torchvision wheels '
+#                       'and install them automatically.')
+#                 print('This may take a while depending on your internet '
+#                       'connection.')
+#                 os.system('python3 -m pip install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp37-cp37m-win_amd64.whl')
+#                 os.system('python3 -m pip install https://download.pytorch.org/whl/cpu/torchvision-0.3.0-cp37-cp37m-win_amd64.whl')
+#             if torch_ver == 'b':
+#                 print('This would download torch and torchvision wheels '
+#                       'and install them automatically.')
+#                 print('This may take a while depending on your internet '
+#                       'connection.')
+#                 os.system('python3 -m pip install https://download.pytorch.org/whl/cu90/torch-1.1.0-cp37-cp37m-win_amd64.whl')
+#                 os.system('python3 -m pip install https://download.pytorch.org/whl/cu90/torchvision-0.3.0-cp37-cp37m-win_amd64.whl')
 
 print('Dependencies installed.')
 print()
@@ -104,8 +104,8 @@ print('What would you like to do?')
 print('1. Fetch Lyrics')
 print('2. Convert Lyrics')
 print('3. Fetch Billboard Charts')
-print('4. Train Model')
-print('5. Sample Output from Model')
+# print('4. Train Model')
+# print('5. Sample Output from Model')
 print('Type "q" to exit.')
 forward = input(': ')
 
@@ -119,19 +119,19 @@ while forward != 'q':
     if forward == '3':
         os.system('python3 charts.py')
 
-    if forward == '4':
-        os.system('python3 trainer.py')
+    # if forward == '4':
+    #     os.system('python3 trainer.py')
 
-    if forward == '5':
-        os.system('python3 sampler.py')
+    # if forward == '5':
+    #     os.system('python3 sampler.py')
 
     print()
     print('Would you like to continue?')
     print('1. Fetch Lyrics')
     print('2. Convert Lyrics')
     print('3. Fetch Billboard Charts')
-    print('4. Train Model')
-    print('5. Sample Output from Model')
+    # print('4. Train Model')
+    # print('5. Sample Output from Model')
     print('Type "q" to exit.')
     forward = input(': ')
 

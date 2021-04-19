963a68fe755e1a15318164d2d0edfedf535f0fe1
Nkosi Lawrence <trinilawrence7@hotmail.com>
2021-04-18 20:27:12 -0400

    Modified how torch is installed
    Moves bumping of torch dependencies to their own requirement files.
    This reduces the need to update the python file, decrease unneeded
    commits.

--- main.py
+++ main.py
@@ -36,94 +36,63 @@ while forward != 'q':
         os.system('python3 charts.py')
 
     if forward == '4' or '5':
-        try:
-            import torch
-            import torchvision
-        except ImportError:
-            print('torch and/or torchvision are missing and are '
-                  'required for training and sampling models.')
-            print('Are you going to be training and sampling models?')
+        print('torch and/or torchvision may be missing or may '
+              'be out of date and are required for training '
+              'and sampling models.')
+        print('Are you going to be training and sampling models?')
+        torch_choice = input('y/n: ')
+        print()
+
+        while not torch_choice in ['y', 'n']:
+            print('Please specify.')
             torch_choice = input('y/n: ')
+            if torch_choice == 'n':
+                print()
+                continue
             print()
 
-            while not torch_choice in ['y', 'n']:
-                print('Please specify.')
-                torch_choice = input('y/n: ')
-                if torch_choice == 'n':
-                    print()
-                    continue
+        if torch_choice == 'y':
+            print('Please specify the operating system you\'re using')
+            print('1. Linux/Windows')
+            print('2. MacOS')
+            os_choice = input(': ')
+            print()
+
+            # Linux/Windows
+            if os_choice == '1':
+                print('Please specify if you want the CPU PyTorch '
+                      'or GPU (NVIDIA - CUDA) PyTorch.')
+                print('The GPU version allows you to do CPU training as well.')
+                print('a. PyTorch CPU')
+                print('b. PyTorch GPU (CUDA)')
+                torch_ver = input(': ')
                 print()
 
-            if torch_choice == 'y':
-                print('Please specify the operating system you\'re using')
-                print('1. Linux')
-                print('2. MacOS')
-                print('3. Windows')
-                os_choice = input(': ')
-                print()
-
-                # Linux
-                if os_choice == '1':
-                    print('Please specify if you want the CPU PyTorch '
-                          'or GPU (NVIDIA - CUDA) PyTorch.')
-                    print('The GPU version allows you to do CPU training as well.')
-                    print('a. PyTorch CPU')
-                    print('b. PyTorch GPU (CUDA)')
-                    torch_ver = input(': ')
-                    print()
-
-                    if torch_ver == 'a':
-                        print('This would download torch and torchvision wheels '
-                              'and install them automatically.')
-                        print('This may take a while depending on your internet '
-                              'connection.')
-                        os.system(
-                            'pip3 install torch==1.5.1+cpu torchvision==0.6.1+cpu '
-                            '-f https://download.pytorch.org/whl/torch_stable.html')
-                    if torch_ver == 'b':
-                        print('This would download torch and torchvision wheels '
-                              'and install them automatically.')
-                        print('This may take a while depending on your internet '
-                              'connection.')
-                        os.system(
-                            'pip3 install torch==1.5.1+cu92 torchvision==0.6.1+cu92 '
-                            '-f https://download.pytorch.org/whl/torch_stable.html')
-
-                # MacOS
-                if os_choice == '2':
-                    print('MacOS binaries don\'t support CUDA, install from '
-                          'source if CUDA is needed.')
-                    print('This would download the CPU binaries for torch and '
-                          'torchvision and install them automatically.')
+                if torch_ver == 'a':
+                    print('This would download torch and torchvision wheels '
+                          'and install them automatically.')
                     print('This may take a while depending on your internet '
                           'connection.')
                     os.system(
-                        'pip3 install torch==1.5.1 torchvision==0.6.1')
+                        'pip3 install -r torch-winux-cpu.txt')
+                if torch_ver == 'b':
+                    print('This would download torch and torchvision wheels '
+                          'and install them automatically.')
+                    print('This may take a while depending on your internet '
+                          'connection.')
+                    os.system(
+                        'pip3 install -r torch-winux-cuda92.txt')
 
-                # Windows
-                if os_choice == '3':
-                    print('Please specify if you want the CPU PyTorch '
-                          'or GPU (NVIDIA - CUDA) PyTorch.')
-                    print('The GPU version allows you to do CPU training as well.')
-                    print('a. PyTorch CPU')
-                    print('b. PyTorch GPU (CUDA)')
-                    torch_ver = input(': ')
-                    if torch_ver == 'a':
-                        print('This would download torch and torchvision wheels '
-                              'and install them automatically.')
-                        print('This may take a while depending on your internet '
-                              'connection.')
-                        os.system(
-                            'pip3 install torch==1.5.1+cpu torchvision==0.6.1+cpu '
-                            '-f https://download.pytorch.org/whl/torch_stable.html')
-                    if torch_ver == 'b':
-                        print('This would download torch and torchvision wheels '
-                              'and install them automatically.')
-                        print('This may take a while depending on your internet '
-                              'connection.')
-                        os.system(
-                            'pip3 install torch==1.5.1+cu92 torchvision==0.6.1+cu92 '
-                            '-f https://download.pytorch.org/whl/torch_stable.html')
+            # MacOS
+            if os_choice == '2':
+                print('MacOS binaries don\'t support CUDA, install from '
+                      'source if CUDA is needed.')
+                print('This would download the CPU binaries for torch and '
+                      'torchvision and install them automatically.')
+                print('This may take a while depending on your internet '
+                      'connection.')
+                os.system(
+                    'pip3 install -r torch-mac-cpu.txt')
 
     if forward == '4':
         os.system('python3 trainer.py')
